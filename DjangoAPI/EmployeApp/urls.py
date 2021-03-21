c3026d3c41b03b034ae13059d995700900c6ec22
Sebastian Fernando Garcia Delgado <sefegade@gmail.com>
2021-03-21 15:13:24 -0500

    Instalacion de Angular 10

--- DjangoAPI/EmployeApp/urls.py
+++ DjangoAPI/EmployeApp/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from EmployeApp import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r'^departament/$',views.departamentApi),
     url(r'^departament/([0-9]+)$',views.departamentApi),
-]
+    url(r'^employe/$',views.employeApi),
+    url(r'^employe/([0-9]+)$',views.employeApi),
+    url(r'^SaveFile$',views.SaveFile),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- DjangoAPI/EmployeApp/views.py
+++ DjangoAPI/EmployeApp/views.py
@@ -6,6 +6,8 @@ from django.http.response import JsonResponse
 from EmployeApp.models import Departaments, Employes
 from EmployeApp.serializers import DepartamentSerializer, EmployeSerializer
 
+from django.core.files.storage import default_storage
+
 @csrf_exempt
 def departamentApi(request,id=0):
     if request.method == 'GET':
@@ -34,3 +36,38 @@ def departamentApi(request,id=0):
         departament= Departaments.objects.get(DepartamentId=id)
         departament.delete()
         return JsonResponse("Registro eliminado", safe= False)
+
+@csrf_exempt
+def employeApi(request,id=0):
+    if request.method == 'GET':
+        employes = Employes.objects.all()
+        employes_serializer = EmployeSerializer(employes, many= True)
+        return JsonResponse(employes_serializer.data, safe= False)
+
+    elif request.method == 'POST':
+        employe_data = JSONParser().parse(request)
+        employe_serializer = EmployeSerializer(data= employe_data)
+        if employe_serializer.is_valid():
+            employe_serializer.save()
+            return JsonResponse("Registro Exitoso", safe= False)
+        return JsonResponse("Registro Fallido", safe= False)
+
+    elif request.method == 'PUT':
+        employe_data = JSONParser().parse(request)
+        employe = Employes.objects.get(EmployeId = employe_data['EmployeId'])
+        employe_serializer = EmployeSerializer(employe, data= employe_data)
+        if employe_serializer.is_valid():
+            employe_serializer.save()
+            return JsonResponse("Actualizacion Correcta", safe= False)
+        return JsonResponse ("Actualizacion Fallida", safe= False)
+        
+    elif request.method == "DELETE":
+        employe= Employes.objects.get(EmployeId=id)
+        employe.delete()
+        return JsonResponse("Registro eliminado", safe= False)
+
+@csrf_exempt
+def SaveFile(request):
+    file = request.FILES['uploadedFile']
+    file_name = default_storage.save(file.name,file)
+    return JsonResponse(file_name,safe=False)
